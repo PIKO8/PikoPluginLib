536b22631d34d1ff16f0eae37bd96b7e9ee94917
PIKO8 <stepanmedvedev80411@gmail.com>
2024-09-29 14:47:19 +0500

    add UText#itemNameComponent(ItemStack)

--- src/main/java/ru/piko/pikopluginlib/Utils/UText.java
+++ src/main/java/ru/piko/pikopluginlib/Utils/UText.java
@@ -2,6 +2,7 @@ package ru.piko.pikopluginlib.Utils;
 
 import net.kyori.adventure.text.Component;
 import org.bukkit.ChatColor;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,5 +19,42 @@ public class UText {
             result.add(color(string));
         }
         return result;
+    }
+
+
+    /**
+     * RU: Возвращает компонент, представляющий название элемента, с учетом его отображаемого имени, ключа перевода и цвета редкости. <br>
+     * EN: Returns a Component representing the item's name, taking into account its display name, translation key, and rarity color. <br> <br>
+     * RU: Более читабельный код: <br>
+     * EN: More readable code:
+     * <pre>{@code public static Component itemNameComponent(ItemStack item) {
+     *     Component component;
+     *     if (item.getItemMeta().hasDisplayName() && !item.displayName().toString().isEmpty()) {
+     *         component = item.displayName();
+     *     } else {
+     *         String translationKey = item.getType().getItemTranslationKey();
+     *         Component itemNameComponent;
+     *         if (translationKey != null) {
+     *             itemNameComponent = Component.translatable(translationKey);
+     *         } else {
+     *             itemNameComponent = Component.text(item.getType().name().toLowerCase());
+     *         }
+     *         itemNameComponent = itemNameComponent.color(item.getRarity().getColor());
+     *         component = itemNameComponent;
+     *     }
+     *     component = component.hoverEvent(item);
+     *     return component;
+     * }}</pre>
+     *
+     * @param item the ItemStack to generate the component for
+     * @return a Component representing the item's name
+     */
+    public static Component itemNameComponent(ItemStack item) {
+        return  (item.getItemMeta().hasDisplayName() && !item.displayName().toString().isEmpty())
+                ? item.displayName()
+                : (item.getType().getItemTranslationKey() != null
+                ? Component.translatable(item.getType().getItemTranslationKey())
+                : Component.text(item.getType().name().toLowerCase()))
+                .color(item.getRarity().getColor());
     }
 }
