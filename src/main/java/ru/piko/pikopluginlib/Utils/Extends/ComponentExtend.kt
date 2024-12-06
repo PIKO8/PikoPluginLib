1bf35312f3eaf3dae8cd1f00282824dd22ccb158
PIKO8 <stepanmedvedev80411@gmail.com>
2024-12-06 13:25:53 +0500

    fix extend

--- src/main/java/ru/piko/pikopluginlib/Utils/Extends/ComponentExtend.kt
+++ src/main/java/ru/piko/pikopluginlib/Utils/Extends/ComponentExtend.kt
@@ -3,12 +3,18 @@ package ru.piko.pikopluginlib.Utils.Extends
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
 
-val mini = MiniMessage.miniMessage()
-
-fun mini(value: String): Component {
-	return mini.deserialize("<italic:false>$value")
+object ComponentExtend {
+	public val mini = MiniMessage.miniMessage()
+	
+	public fun mini(value: String): Component {
+		return mini.deserialize("<italic:false>$value")
+	}
+	
+	public fun mini(vararg values: String): List<Component> {
+		return values.map { mini(it) }
+	}
 }
 
-fun mini(vararg values: String): List<Component> {
-	return values.map { mini(it) }
+public fun String.mini(): Component {
+	return ComponentExtend.mini(this)
 }
